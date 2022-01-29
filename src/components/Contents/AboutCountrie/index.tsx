18c92cc9bf518fa5bda7d4dd3d3f8cbe34664981
ManuelMolina02 <manuelanmolina@gmail.com>
2022-01-29 20:38:49 -0300

    atualizando estilizadaçao | conjunto de dados | estrutura do componente

--- src/components/Contents/AboutCountrie/index.tsx
+++ src/components/Contents/AboutCountrie/index.tsx
@@ -11,22 +11,45 @@ export function AboutCountrie({ countrie }: aboutCountrieProps) {
 
     <div className={styles.profileContainer}>
 
-      <img src={countrie.url_flag} alt="" />
+      <div className={styles.profileBanner}>
+        <img src={countrie.url_flag} alt="" />
 
-      <h3>
-        {countrie.name} | {countrie.initials}
-      </h3>
+        <h3>
+          {countrie.name} | {countrie.initials}
+        </h3>
+      </div>
 
       <div className={styles.profileContent}>
-        <div>
-          <p>Capital:</p>
-          <h5>{countrie.capital}</h5>
+        <div className={styles.row}>
+
+          <div>
+            <p>Capital:</p>
+            <h5>{countrie.capital}</h5>
+          </div>
+
+          <div>
+            <p>Continente:</p>
+            <h5>{countrie.continent}</h5>
+          </div>
         </div>
 
         <div>
-          <p>Continente:</p>
-          <h5>{countrie.continent}</h5>
+          <div>
+            <p>Índice de Complixadade <br />Econômica (ICE) </p>
+            <h5>{countrie.ice_position}</h5>
+          </div>
+
+          <div>
+            <p>Expectativa de Vida</p>
+            <h5>{countrie.life_expectancy} anos</h5>
+          </div>
+
+          <div>
+            <p>Despesa com Saúde <br />(Per Capta)</p>
+            <h5>$ {countrie.health_expenditure_percap}</h5>
+          </div>
         </div>
+
       </div>
 
     </div>
